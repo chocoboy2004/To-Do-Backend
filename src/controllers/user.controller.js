173a37ee92018724140a50e60686d7471dea7945
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-07-10 20:43:24 +0530

    creating userLogin controller & route, also a function that generates the refresh & access tokens

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,48 +3,99 @@ import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import User from "../models/user.model.js";
 
-const registerUser = AsyncHandler(async(req, res) => {
-    const { firstname, lastname, email, password } = req.body
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const getUser = await User.findById(userId).select("-password");
+    const accessToken = getUser.generateAccessToken();
+    const refreshToken = getUser.generateRefreshToken();
 
-    if (!firstname || !lastname || !email || !password) {
-        throw new ApiError(200, "No fields should be empty")
-    }
+    getUser.refreshToken = refreshToken;
+    await getUser.save({ validateBeforeSave: false });
 
-    if (!email.includes("@")) {
-        throw new ApiError(400, "Invalid email")
-    }
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong while generating tokens");
+  }
+};
 
-    if (password.length < 8) {
-        throw new ApiError(400, "Password must be atleast 8 digits")
-    }
+const registerUser = AsyncHandler(async (req, res) => {
+  const { firstname, lastname, email, password } = req.body;
 
-    const response = await User.findOne({ email })
-    if (response) {
-        throw new ApiError(400, "Existed user")
-    }
+  if (!firstname || !lastname || !email || !password) {
+    throw new ApiError(200, "No fields should be empty");
+  }
 
-    const user = await User.create({
-        firstname: firstname.trim().toLowerCase(),
-        lastname: lastname.trim().toLowerCase(),
-        email: email.trim().toLowerCase(),
-        password: password.toString()
-    })
+  if (!email.includes("@")) {
+    throw new ApiError(400, "Invalid email");
+  }
 
-    const createdUser = await User.findById(user._id).select("-password")
+  if (password.length < 8) {
+    throw new ApiError(400, "Password must be atleast 8 digits");
+  }
 
-    if (!createdUser) {
-        throw new ApiError(500, "Something went wrong while registering the user")
-    }
+  const response = await User.findOne({ email });
+  if (response) {
+    throw new ApiError(400, "Existed user");
+  }
 
-    return res
+  const user = await User.create({
+    firstname: firstname.trim().toLowerCase(),
+    lastname: lastname.trim().toLowerCase(),
+    email: email.trim().toLowerCase(),
+    password: password.toString(),
+  });
+
+  const createdUser = await User.findById(user._id).select("-password");
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  return res
     .status(201)
-    .json(
-        new ApiResponse(
-            200,
-            createdUser,
-            "User registered successfully!"
-        )
-    )
-})
+    .json(new ApiResponse(200, createdUser, "User registered successfully!"));
+});
 
-export { registerUser }
+const loginUser = AsyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  if(!email || !password) {
+    throw new ApiError(400, "No fields should be empty")
+  }
+
+  if (!email.includes("@")) {
+    throw new ApiError(400, "Invalid email")
+  }
+
+  if (password.length < 8) {
+    throw new ApiError(400, "Password must be at least 8 digits")
+  }
+
+  const response = await User.findOne({ email })
+  if (!response) {
+    throw new ApiError(401, "You haven't registered yet! Please register yourself")
+  }
+
+  const isValidPass = await response.checkPassword(password.toString(), response.password)
+  if (!isValidPass) {
+    throw new ApiError(401, "Invalid password")
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(response._id)
+  const user = await User.findById(response._id).select("-password -refrehToken")
+
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
+  return res
+  .status(201)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+    new ApiResponse(200, user, "User loggged in successfully!")
+  )
+});
+
+export { registerUser, loginUser };
