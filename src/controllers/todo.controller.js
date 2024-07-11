11b491b5ec938f378e1e6ca59dfcd5116cc64989
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-07-11 20:03:25 +0530

    creating deleteTodo controller and its route

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -2,6 +2,7 @@ import AsyncHandler from "../utils/AsyncHandler.js"
 import ApiError from "../utils/ApiError.js"
 import ApiResponse from "../utils/ApiResponse.js"
 import Todo from "../models/todo.model.js"
+import mongoose from "mongoose"
 
 const createTodo = AsyncHandler(async(req, res) => {
     const { name, description } = req.body;
@@ -33,6 +34,27 @@ const createTodo = AsyncHandler(async(req, res) => {
     )
 })
 
+const deleteTodo = AsyncHandler(async(req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        throw new ApiError(400, "Id is required")
+    }
+
+    const response = await Todo.findOne({ _id: new mongoose.Types.ObjectId(id) })
+    if (!response) {
+        throw new ApiError(400, "Invalid todo id or request")
+    }
+
+    await Todo.deleteOne(response._id)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, {}, "Todo deleted successfully!")
+    )
+})
+
 export {
-    createTodo
+    createTodo,
+    deleteTodo
 }
--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
 import { 
-    createTodo 
+    createTodo, 
+    deleteTodo
 } from "../controllers/todo.controller.js";
 
 const router = Router()
 
 router.route("/create").post(verifyJWT, createTodo);
+router.route("/delete/:id").post(deleteTodo);
 
 export default router;
