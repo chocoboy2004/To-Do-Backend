770b53563b494625a33ea0a1bdba41a990228e44
chocoboy2004 <iamrahulghoshinfo@gmail.com>
2024-07-11 23:12:56 +0530

    creating toggle controller and route. Also fix some changes in the deleteTodo

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -40,7 +40,7 @@ const deleteTodo = AsyncHandler(async(req, res) => {
         throw new ApiError(400, "Id is required")
     }
 
-    const response = await Todo.findOne({ _id: new mongoose.Types.ObjectId(id) })
+    const response = await Todo.findById(new mongoose.Types.ObjectId(id))
     if (!response) {
         throw new ApiError(400, "Invalid todo id or request")
     }
@@ -54,7 +54,90 @@ const deleteTodo = AsyncHandler(async(req, res) => {
     )
 })
 
+const updateTodo = AsyncHandler(async(req, res) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    if (!id) {
+        throw new ApiError(400, "Id is required")
+    }
+    
+    if (!name && !description) {
+        throw new ApiError(400, "todo name or description is needed")
+    }
+
+    const response = await Todo.findById(new mongoose.Types.ObjectId(id))
+
+    if (!response) {
+        throw new ApiError(400, "Invalid todo id")
+    }
+
+    response.name = name.trim() !== "" ? name.trim() : response.name
+    response.description = description.trim() !== "" ? description.trim() : response.description
+    await response.save({ validateBeforeSave: false })
+
+    const updatedTodo = await Todo.findById(response._id)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, updatedTodo, "Todo updated successfully!"
+        )
+    )
+})
+
+const displayAllTodos = AsyncHandler(async(req, res) => {
+    const user = req.user._id;
+    if (!user) {
+        throw new ApiError(404, "Token got expired")
+    }
+
+    const todo = await Todo.find({ createdBy: user })
+    if (!todo) {
+        throw new ApiError(404, "You have not listed any todo")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            todo,
+            "All todos are fetched successfully!"
+        )
+    )
+})
+
+const toggle = AsyncHandler(async(req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        throw new ApiError(400, "Id is required")
+    }
+
+    const todo = await Todo.findById(new mongoose.Types.ObjectId(id))
+    if (!todo) {
+        throw new ApiError(400, "Invalid todo id or todo is not created yet")
+    }
+
+    todo.isCompleted = !todo.isCompleted
+    await todo.save({ validateBeforeSave: false })
+
+    const response = await Todo.findById(todo._id)
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, response, "Todo status has been updated successfully!"
+        )
+    )
+})
+
 export {
     createTodo,
-    deleteTodo
+    deleteTodo,
+    updateTodo,
+    displayAllTodos,
+    toggle
 }
--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -2,12 +2,18 @@ import { Router } from "express";
 import verifyJWT from "../middlewares/auth.middleware.js";
 import { 
     createTodo, 
-    deleteTodo
+    deleteTodo,
+    updateTodo,
+    displayAllTodos,
+    toggle
 } from "../controllers/todo.controller.js";
 
 const router = Router()
 
 router.route("/create").post(verifyJWT, createTodo);
-router.route("/delete/:id").post(deleteTodo);
+router.route("/delete/:id").post(verifyJWT, deleteTodo);
+router.route("/update/:id").patch(verifyJWT, updateTodo);
+router.route("/display-todos").get(verifyJWT, displayAllTodos);
+router.route("/toggle/:id").patch(verifyJWT, toggle);
 
 export default router;
